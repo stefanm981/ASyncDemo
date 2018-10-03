7f5aa3a9a7c9967978267fae49dd01467a6709fc
stefanm981 <32268762+stefanm981@users.noreply.github.com>
2018-10-03 16:42:37 +0300

    Simple demo of async program with 5 threads doing some operations parallel with the main thread that read lines from the console (without blocking it).

---
+++ ASyncDemo/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ASyncDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(() =>
+            {
+                var result = NumberOfPrimesInInterval(2, 200000);
+
+                Console.WriteLine(result + " comes from thread #" 
+                    + Thread.CurrentThread.ManagedThreadId);
+            });
+            thread.Start();
+
+            for (int i = 0; i < 5; i++)
+            {
+                var task = Task.Run(() => NumberOfPrimesInInterval(2, 200000));
+                task.ContinueWith((taskResult) => Console.WriteLine(taskResult.Result
+                     + " comes from thread #"
+                    + Thread.CurrentThread.ManagedThreadId));
+            }
+
+            Console.WriteLine("The main thread is #" + Thread.CurrentThread.ManagedThreadId);
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "exit")
+                {
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+
+        public static int NumberOfPrimesInInterval(int min, int max)
+        {
+            int count = 0;
+
+            for (int i = min; i <= max; i++)
+            {
+                bool isPrime = true;
+
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
